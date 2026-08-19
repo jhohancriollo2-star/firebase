024f7d45afa9891d84e5572faf0cc1d94da1a6fa
Andres Landines <cardosoandres331@gmail.com>
2026-08-18 19:56:37 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'senaFirebase',
+  webDir: 'www'
+};
+
+export default config;
